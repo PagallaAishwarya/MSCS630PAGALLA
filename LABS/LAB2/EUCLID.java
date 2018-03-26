ebf71fc08ba6b767293f11ea39a43180cea0af4b
Aishwarya <aishwarya.pagalla1@marist.edu>
2018-03-26 13:09:55 -0400

    updated lab2

--- LABS/LAB2/EUCLID.java
+++ LABS/LAB2/EUCLID.java
@@ -6,38 +6,35 @@ course:MSCS630
 assignment due:02/07/2018
 version:1.0
 */
-package gcd;
 
 import java.util.Scanner;
 
 public class EUCLID {
-	public static void main(String args[]){
+   public static void main(String args[]){
+	//prompts user to enter input
+	Scanner S = new Scanner(System.in);
+        System.out.println("enter values for a and b");
+        long a = S.nextLong();
+        long b = S.nextLong();
+	long g = gcd(a,b);
+        //condition - a always greater than b for a/b
+	if (a>=b){
+          System.out.println("GCD of  " + a + " & " + b +" is : " + g);}
+        else {
+        //swap if a less than b;
+          long temp = a;
+          a = b;
+          b = temp;
+          System.out.println("GCD of  " + a + " & " + b +" is : " + g);
+   }
+      }
 		
-		//prompts user to enter input
-		Scanner S = new Scanner(System.in);
-		System.out.println("enter values for a and b");
-		long a = S.nextLong();
-		long b = S.nextLong();
-		long g = gcd(a,b);
-		//condition - a always greater than b for a/b
-		if (a>=b){
-		System.out.println("GCD of  " + a + " & " + b +" is : " + g);}
-			else {
-		//swap if a less than b;
-			    long temp = a;
-			a = b;
-			b = temp;
-			System.out.println("GCD of  " + a + " & " + b +" is : " + g);
-		}
-	}
-		
-	private static long gcd(long a, long b) { 
-			//base condition - if b = 0
-			if(b == 0){
-			return a; }
-			
-		return gcd(b, a%b); 
-		
+   private static long gcd(long a, long b) { 
+   //base condition - if b = 0
+    if(b == 0){
+    return a; 
+    }
+    return gcd(b, a%b);	
 	}
 	
 }
