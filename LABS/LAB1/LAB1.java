210358a7a33c80d760bf1e2fd7bcee7ecedcc916
Aishwarya <aishwarya.pagalla1@marist.edu>
2018-03-26 13:26:14 -0400

    updated lab1

--- LABS/LAB1/LAB1.java
+++ LABS/LAB1/LAB1.java
@@ -7,48 +7,40 @@ course:MSCS630
 assignment due:02/07/2018
 version:1.0
  */
-package javaapplication4;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Aishw
- */
 public class LAB1 {
-
-    /**
-     * @param args the command line arguments
-     */
-  public static void main(String[] args) {
-  Scanner scan = new Scanner(System.in);
-  while(scan.hasNext()) {
+ 
+ public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     while(scan.hasNext()) {
      String plainText = scan.nextLine();
      //reading Input String
      plainText = plainText.toUpperCase();
      //making input caseSensitive
      int[] o = str2int(plainText);
      //refering to method
-     for(int i=0;i< o.length;i++) {
-        System.out.print(o[i] + " ");
-     }
-        System.out.println();
-     }
+    for(int i=0;i< o.length;i++) {
+      System.out.print(o[i] + " ");
+       }
+      System.out.println();
+       }
   }  
-  //static method
-  static int[]  str2int(String plainText){
+   // method str2int converts the string input to integer
+ static int[]  str2int(String plainText){
       
-  int[] output = new int[plainText.length()];
-  plainText = plainText.toUpperCase();
-     for(int i=0;i<plainText.length();i++) { 
-     char c = plainText.charAt(i);
+    int[] output = new int[plainText.length()];
+    plainText = plainText.toUpperCase();
+    for(int i=0;i<plainText.length();i++) { 
+    char c = plainText.charAt(i);
      // varaiable gets ascii code of input entered
-     if(c == 32)
-     output[i] = 26;
-     else
-        output[i] = (((int) c)-65);
+    if(c == 32)
+       output[i] = 26;
+    else
+       output[i] = (((int) c)-65);
      }
-     return output;
+    return output;
      //returns the output value 
      }
   }  
