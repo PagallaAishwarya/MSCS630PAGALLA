9cbec6e02fbb1ef84f85eba50abc297b2d9083e4
Aishwarya <aishwarya.pagalla1@marist.edu>
2018-04-13 14:58:31 -0400

    Update DriverAES.java

--- LABS/LAB4/DriverAES.java
+++ LABS/LAB4/DriverAES.java
@@ -1,12 +1,18 @@
-package h;
-
+/**
+file:DriverAES.java
+author:Aishwarya Pagalla
+assignment: Lab 4
+course:MSCS630
+assignment:04/13/2018
+version:1.0 
+*/
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 
 public class DriverAES {
 
 		  public static void main(String[] args) {
-		    Scanner in = new Scanner(new BufferedInputStream(System.in));
+		   Scanner in = new Scanner(new BufferedInputStream(System.in));
 		    System.out.println("enter input");
 		    while (in.hasNext()) {
 		      String input = in.nextLine();
