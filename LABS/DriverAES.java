5ebebd7946c0efbd6538e2067998dcb9123f3c37
Aishwarya <aishwarya.pagalla1@marist.edu>
2018-05-04 11:08:56 -0400

    Update DriverAES.java

--- LABS/DriverAES.java
+++ LABS/DriverAES.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package LAB5;
 
 /**
- *
- * @author Anurag
- */
+file:AESCipher.java
+author:Aishwarya Pagalla
+assignment: Lab 5
+course:MSCS630
+assignment :05/04/2018
+version:1.0 
+*/
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 
